75d1b8ded5ced0c09ee26a3e71d3db76a5ecebdb
makvanayash2112 <112868826+makvanayash2112@users.noreply.github.com>
2026-05-23 10:50:33 +0530

    some chnages

---
+++ server/utils/ondc-public-keys.ts
@@ -0,0 +1,9 @@
+export const ONDC_PUBLIC_KEYS: Record<string, string> = {
+
+  "pramaan.ondc.org/beta/preprod/mock/buyer":
+    "VeKKg8tUxcZ00SB1tvkwYDrZ2VnQ0rQ4c/KyzyBVMMY=",
+
+  "pp-ondc-buyer.digicraft.ai":
+    "VeKKg8tUxcZ00SB1tvkwYDrZ2VnQ0rQ4c/KyzyBVMMY=",
+
+};
--- server/utils/ondc-registry.ts
+++ server/utils/ondc-registry.ts
@@ -1,51 +1,74 @@
-import axios from "axios";
-import https from "https";
+// import axios from "axios";
+// import https from "https";
 
-const httpsAgent = new https.Agent({
-  rejectUnauthorized: false,
-});
+// const httpsAgent = new https.Agent({
+//   rejectUnauthorized: false,
+// });
+
+// export async function fetchPublicKey(
+//   subscriberId: string
+// ): Promise<string | null> {
+
+//   try {
+
+//     const response = await axios.post(
+//       "https://registry.ondc.org/v2.0/lookup",
+//       {
+//         subscriber_id: subscriberId,
+//       },
+//       {
+//         httpsAgent,
+//       }
+//     );
+
+//     const subscriber =
+//       response.data?.[0];
+
+//     if (!subscriber) {
+
+//       console.error(
+//         "[ondc-registry] Subscriber not found"
+//       );
+
+//       return null;
+//     }
+
+//     console.log(
+//       "[ondc-registry] Public key fetched"
+//     );
+
+//     return subscriber.signing_public_key;
+
+//   } catch (err) {
+
+//     console.error(
+//       "[ondc-registry] Failed lookup",
+//       err
+//     );
+
+//     return null;
+//   }
+// }
+
+import { ONDC_PUBLIC_KEYS }
+    from "./ondc-public-keys";
 
 export async function fetchPublicKey(
-  subscriberId: string
+    subscriberId: string
 ): Promise<string | null> {
 
-  try {
+    const key =
+        ONDC_PUBLIC_KEYS[subscriberId];
 
-    const response = await axios.post(
-      "https://registry.ondc.org/v2.0/lookup",
-      {
-        subscriber_id: subscriberId,
-      },
-      {
-        httpsAgent,
-      }
-    );
+    if (!key) {
 
-    const subscriber =
-      response.data?.[0];
+        console.error(
+            "[ondc] Public key not found:",
+            subscriberId
+        );
 
-    if (!subscriber) {
-
-      console.error(
-        "[ondc-registry] Subscriber not found"
-      );
-
-      return null;
+        return null;
     }
 
-    console.log(
-      "[ondc-registry] Public key fetched"
-    );
-
-    return subscriber.signing_public_key;
-
-  } catch (err) {
-
-    console.error(
-      "[ondc-registry] Failed lookup",
-      err
-    );
-
-    return null;
-  }
+    return key;
 }
