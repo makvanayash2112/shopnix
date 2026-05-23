8803c1553ddeaa2a12fb0d2baed88b914dc3f8e6
makvanayash2112 <112868826+makvanayash2112@users.noreply.github.com>
2026-05-23 10:37:50 +0530

    some changes

--- server/utils/ondc-registry.ts
+++ server/utils/ondc-registry.ts
@@ -1,4 +1,9 @@
 import axios from "axios";
+import https from "https";
+
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 export async function fetchPublicKey(
   subscriberId: string
@@ -6,27 +11,38 @@ export async function fetchPublicKey(
 
   try {
 
-    const response =
-      await axios.post(
-        "https://registry.ondc.org/v2.0/lookup",
-        {
-          subscriber_id: subscriberId,
-        }
-      );
+    const response = await axios.post(
+      "https://registry.ondc.org/v2.0/lookup",
+      {
+        subscriber_id: subscriberId,
+      },
+      {
+        httpsAgent,
+      }
+    );
 
     const subscriber =
       response.data?.[0];
 
     if (!subscriber) {
+
+      console.error(
+        "[ondc-registry] Subscriber not found"
+      );
+
       return null;
     }
+
+    console.log(
+      "[ondc-registry] Public key fetched"
+    );
 
     return subscriber.signing_public_key;
 
   } catch (err) {
 
     console.error(
-      "[ondc-registry] Failed to fetch public key",
+      "[ondc-registry] Failed lookup",
       err
     );
 
